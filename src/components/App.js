0d983b731728ee4ad2326d86736dcf6ac08d058b
mvpache <mvpache@msn.com>
2018-04-07 19:45:30 -0700

    body and navbar set up

--- src/components/App.js
+++ src/components/App.js
@@ -1,4 +1,7 @@
 import React, { Component } from 'react';
+
+import NavBar from './NavBar';
+import Body from './Body';
 import './App.css';
 
 class App extends Component {
@@ -8,9 +11,8 @@ class App extends Component {
         <header className="App-header">
           <h1 className="App-title">Welcome to React</h1>
         </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <NavBar />
+        <Body />
       </div>
     );
   }
---
+++ src/components/Body.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class Body extends Component {
+  render() {
+    return (
+      <div>
+        Will need list of new and upcoming movies/API calls etc
+      </div>
+    )
+  }
+}
+
+export default Body;
