527004deb444ac509d1a44315fd80fb18d17cd6a
mvpache <mvpache@msn.com>
2018-06-20 12:17:00 -0700

    SSR testing

--- client/src/components/NavBar.js
+++ client/src/components/NavBar.js
@@ -6,26 +6,29 @@ import Search from './navComponents/Search';
 
 const Wrapper = styled.div`
   background: Black;
-  display:inline-block;
+  display: inline-block;
   width: 100%;
   margin: 0;
 `;
 
 const Title = styled.h1`
   color: white;
-`
+`;
 
 class NavBar extends Component {
-
   render() {
     return (
       <Wrapper>
-        <Title onClick={() => { this.props.history.push('/')}}>
-        Movie Ramble
+        <Title
+          onClick={() => {
+            this.props.history.push('/');
+          }}
+        >
+          {process.env.APIKEY}
         </Title>
-        <Search/>
+        <Search />
       </Wrapper>
-    )
+    );
   }
 }
 
